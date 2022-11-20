c5449801394025032af7cb3e78425e0f858fc1dc
Vo Huu Nam Truong <namtruong0900@gmail.com>
2022-11-20 19:28:40 +0700

    Test and failed

---
+++ MFCC_Kcluster.m
@@ -0,0 +1,149 @@
+function [vector_K2, vector_K3, vector_K4, vector_K5] =    MFCC_Kcluster( folderName, fileName, fileFolderLength )
+N_MFCC = 13;
+
+
+
+
+
+
+
+
+    individual_folderName=folderName((1-1)*5+1:5*1);
+    audioName = ['THHL\' individual_folderName '\'  fileName];
+    
+    [y, Fs] = audioread(audioName); % doc du lieu tu file .wav
+    y = y / max(abs(y)); % chuan hoa bien do ve [0;1]
+    
+    %Phan tich tieng noi khoang lang
+    frame_indexes = DrawGraph(audioName);       
+    
+    N_start=frame_indexes(1);
+    N_end=frame_indexes(length(frame_indexes));
+    
+    %chia khoang tieng noi thanh 3 phan bang nhau va lay phan giua
+    khoangchia=(N_end-N_start)/3;
+    
+    khung_chia_start = round(Fs * (N_start+khoangchia));
+    khung_chia_end = round(Fs * (N_start+2*khoangchia));
+
+        khung_chia = y( khung_chia_start : khung_chia_end );
+
+        
+        f_d = 0.025; % do dai cua moi frame 
+        n = f_d * Fs;  % so luong mau trong moi frame
+        
+        frames = DivFrame(khung_chia, n);
+        
+
+
+        temp_frame = frames(1, : );
+        coeffs = melcepst(temp_frame,Fs, 'M', N_MFCC, floor(3*log(Fs)), 30 , 15, 0, 0.5);%'WindowLength',25,'OverlapLength',12,'numcoeffs',12
+        
+        K2 = kmeans(coeffs, 2).';
+        K3 = kmeans(coeffs, 3).';
+        K4 = kmeans(coeffs, 4).';
+        K5 = kmeans(coeffs, 5).';
+        
+        
+        
+
+
+        
+    for j = 2: size(frames,1)
+        temp_frame = frames(j, : );
+        coeffs = melcepst(temp_frame,Fs, 'M', N_MFCC, floor(3*log(Fs)), 30 , 15, 0, 0.5);%'WindowLength',25,'OverlapLength',12,'numcoeffs',12
+        K2 = K2 + kmeans(coeffs, 2).';
+        K3 = K3 + kmeans(coeffs, 3).';
+        K4 = K4 + kmeans(coeffs, 4).';
+        K5 = K5 + kmeans(coeffs, 5).';
+    end
+        
+        %vector dac trung 
+        frame_mfcc_K2 = K2./size(frames,1);
+        frame_mfcc_K3 = K3./size(frames,1);
+        frame_mfcc_K4 = K4./size(frames,1);
+        frame_mfcc_K5 = K5./size(frames,1);
+        
+
+        sum_K2 = frame_mfcc_K2;
+        sum_K3 = frame_mfcc_K3;
+        sum_K4 = frame_mfcc_K4;
+        sum_K5 = frame_mfcc_K5;
+        
+
+
+
+
+
+
+
+      
+  
+for i=2:fileFolderLength
+
+    
+    individual_folderName=folderName((i-1)*5+1:5*i);
+    audioName = ['THHL\' individual_folderName '\'  fileName];
+    
+    [y, Fs] = audioread(audioName); % doc du lieu tu file .wav
+    y = y / max(abs(y)); % chuan hoa bien do ve [0;1]
+    
+    %Phan tich tieng noi khoang lang
+    frame_indexes = DrawGraph(audioName);       
+    
+    N_start=frame_indexes(1);
+    N_end=frame_indexes(length(frame_indexes));
+    
+    %chia khoang tieng noi thanh 3 phan bang nhau va lay phan giua
+    khoangchia=(N_end-N_start)/3;
+    
+    khung_chia_start = round(Fs * (N_start+khoangchia));
+    khung_chia_end = round(Fs * (N_start+2*khoangchia));
+
+        khung_chia = y( khung_chia_start : khung_chia_end );
+%          plot(khung_chia);
+        
+        f_d = 0.025; % do dai cua moi frame 
+        n = f_d * Fs;  % so luong mau trong moi frame
+        
+        frames = DivFrame(khung_chia, n);
+        
+%       individual_frame_mfcc = zeros(1, N_MFCC);
+
+        temp_frame = frames(1, : );
+        coeffs = melcepst(temp_frame,Fs, 'M', N_MFCC, floor(3*log(Fs)), 30 , 15, 0, 0.5);%'WindowLength',25,'OverlapLength',12,'numcoeffs',12
+        
+        K2 = kmeans(coeffs, 2).';
+        K3 = kmeans(coeffs, 3).';
+        K4 = kmeans(coeffs, 4).';
+        K5 = kmeans(coeffs, 5).';
+        
+    for j = 2: size(frames,1)
+        temp_frame = frames(j, : );
+        coeffs = melcepst(temp_frame,Fs, 'M', N_MFCC, floor(3*log(Fs)), 30 , 15, 0, 0.5);%'WindowLength',25,'OverlapLength',12,'numcoeffs',12
+        K2 = K2 + kmeans(coeffs, 2).';
+        K3 = K3 + kmeans(coeffs, 3).';
+        K4 = K4 + kmeans(coeffs, 4).';
+        K5 = K5 + kmeans(coeffs, 5).';
+    end
+        
+        %vector dac trung 
+        frame_mfcc_K2 = K2./size(frames,1);
+        frame_mfcc_K3 = K3./size(frames,1);
+        frame_mfcc_K4 = K4./size(frames,1);
+        frame_mfcc_K5 = K5./size(frames,1);
+        
+
+        sum_K2 = sum_K2 + frame_mfcc_K2;
+        sum_K3 = sum_K3 + frame_mfcc_K3;
+        sum_K4 = sum_K4 + frame_mfcc_K4;
+        sum_K5 = sum_K5 + frame_mfcc_K5;
+end
+
+vector_K2 = sum_K2./fileFolderLength; 
+vector_K3 = sum_K3./fileFolderLength; 
+vector_K4 = sum_K4./fileFolderLength; 
+vector_K5 = sum_K5./fileFolderLength;
+
+end
+
---
+++ mainMFCC_Kcluster.m
@@ -0,0 +1,82 @@
+close all;
+clear;
+
+folderName_THHL = ['01MDA', '02FVA', '03MAB', '04MHB', '05MVB', '06FTB', '07FTC', '08MLD', '09MPD', '10MSD','11MVD','12FTD','14FHH', '15MMH','16FTH','17MTH','18MNK', '19MXK', '20MVK', '21MTL', '22MHL'];
+folderName_THKT = ['23MTL', '24FTL', '25MLM', '27MCM', '28MVN', '29MHN', '30FTN', '32MTP', '33MHP', '34MQP','35MMQ','36MAQ','37MDS', '38MDS','39MTS','40MHS','41MVS', '42FQT', '43MNT', '44MTT', '45MDV'];
+
+%So folder trong THHL va THKT
+fileFolderLength = 21;
+
+K2 = 2;
+K3 = 3;
+K4 = 4;
+K5 = 5;
+
+%Huan luyen vector dac trung
+[ vector_K2_a, vector_K3_a, vector_K4_a,vector_K5_a ] = MFCC_Kcluster(folderName_THHL, 'a.wav' , fileFolderLength);
+[ vector_K2_u, vector_K3_u, vector_K4_u,vector_K5_u ] = MFCC_Kcluster(folderName_THHL, 'u.wav' , fileFolderLength);
+[ vector_K2_i, vector_K3_i, vector_K4_i,vector_K5_i ] = MFCC_Kcluster(folderName_THHL, 'i.wav' , fileFolderLength);
+[ vector_K2_e, vector_K3_e, vector_K4_e,vector_K5_e ] = MFCC_Kcluster(folderName_THHL, 'e.wav' , fileFolderLength);
+[ vector_K2_o, vector_K3_o, vector_K4_o,vector_K5_o ] = MFCC_Kcluster(folderName_THHL, 'o.wav' , fileFolderLength);
+
+f2 = figure('Name', 'Vector dac trung MFCC');
+hold on;
+plot(vector_K5_a);
+plot(vector_K5_u);
+plot(vector_K5_i);
+plot(vector_K5_e);
+plot(vector_K5_o);
+title('Vector dac trung MFCC');
+legend({'/a/','/u/', '/i/', '/e/', '/o/'},'Location','north');
+hold off;
+
+[check_a_a_K5, check_a_u_K5, check_a_i_K5, check_a_e_K5, check_a_o_K5] = MFCCsosanh_Kcluster( folderName_THKT, 'a.wav', fileFolderLength, K2 , vector_K5_a, vector_K5_u, vector_K5_i, vector_K5_e, vector_K5_o);
+[check_u_a_K5, check_u_u_K5, check_u_i_K5, check_u_e_K5, check_u_o_K5] = MFCCsosanh_Kcluster( folderName_THKT, 'u.wav', fileFolderLength, K2 , vector_K5_a, vector_K5_u, vector_K5_i, vector_K5_e, vector_K5_o);
+[check_i_a_K5, check_i_u_K5, check_i_i_K5, check_i_e_K5, check_i_o_K5] = MFCCsosanh_Kcluster( folderName_THKT, 'i.wav', fileFolderLength, K2 , vector_K5_a, vector_K5_u, vector_K5_i, vector_K5_e, vector_K5_o);
+[check_e_a_K5, check_e_u_K5, check_e_i_K5, check_e_e_K5, check_e_o_K5] = MFCCsosanh_Kcluster( folderName_THKT, 'e.wav', fileFolderLength, K2 , vector_K5_a, vector_K5_u, vector_K5_i, vector_K5_e, vector_K5_o);
+[check_o_a_K5, check_o_u_K5, check_o_i_K5, check_o_e_K5, check_o_o_K5] = MFCCsosanh_Kcluster( folderName_THKT, 'o.wav', fileFolderLength, K2 , vector_K5_a, vector_K5_u, vector_K5_i, vector_K5_e, vector_K5_o);
+
+%<--- khoi tao bang thong ke
+check = zeros(5,5);
+
+check(1,1) = check_a_a_K5;
+check(1,2) = check_a_u_K5;
+check(1,3) = check_a_i_K5;
+check(1,4) = check_a_e_K5;
+check(1,5) = check_a_o_K5;
+
+check(2,1) = check_u_a_K5;
+check(2,2) = check_u_u_K5;
+check(2,3) = check_u_i_K5;
+check(2,4) = check_u_e_K5;
+check(2,5) = check_u_o_K5;
+
+check(3,1) = check_i_a_K5;
+check(3,2) = check_i_u_K5;
+check(3,3) = check_i_i_K5;
+check(3,4) = check_i_e_K5;
+check(3,5) = check_i_o_K5;
+
+check(4,1) = check_e_a_K5;
+check(4,2) = check_e_u_K5;
+check(4,3) = check_e_i_K5;
+check(4,4) = check_e_e_K5;
+check(4,5) = check_e_o_K5;
+
+check(5,1) = check_o_a_K5;
+check(5,2) = check_o_u_K5;
+check(5,3) = check_o_i_K5;
+check(5,4) = check_o_e_K5;
+check(5,5) = check_o_o_K5;
+
+h = figure;
+data = check;
+u = uitable('Position',[20 20 500 70],'data',data);
+u.RowName = {'a','u','i','e','o'};
+u.ColumnName = {'a','u','i','e','o'};
+table_extent = get(u,'Extent');
+set(u,'Position',[1 1 table_extent(3) table_extent(4)])
+figure_size = get(h,'outerposition');
+desired_fig_size = [figure_size(1) figure_size(2) table_extent(3)+15 table_extent(4)+65+20];
+set(h,'outerposition', desired_fig_size);
+title('K2');
